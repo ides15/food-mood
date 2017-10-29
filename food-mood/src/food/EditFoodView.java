d06ba7ce9bce328e5e8b1cb768ab1ff265a54d35
kyleisryan <kjryan570@gmail.com>
2017-10-29 17:20:20 -0400

    added addfood and editfood controllers

--- food-mood/src/food/EditFoodView.java
+++ food-mood/src/food/EditFoodView.java
@@ -5,10 +5,42 @@
  */
 package food;
 
+import database.Food_Table;
+import javax.swing.event.ListSelectionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import models.Food;
+
 /**
  *
  * @author Kyle
  */
-public class EditFoodView {
+public class EditFoodView extends JFrame {
     
+    private Food food;
+    private final Food_Table db;
+    private EditFoodPanel editFoodPanel;
+    
+    public EditFoodView(Food_Table db){
+        super("Food");
+        this.db = db;
+        
+        setSize(400, 400);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+        editFoodPanel = new EditFoodPanel();
+        add(editFoodPanel);
+    }
+    
+    public void addUpdateButtonListener(ActionListener al){
+        getEditFoodPanel().getUpdateButton().addActionListener(al);
+    }
+    
+    public EditFoodPanel getEditFoodPanel(){
+        return this.editFoodPanel;
+    }
+    
+    public void setEditFoodPanel(AddFoodPanel EditFoodPanel){
+        this.editFoodPanel = editFoodPanel;
+    }
 }
