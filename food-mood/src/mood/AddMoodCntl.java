c83d4c4cc5fce668bc8a9cafd1b2fd9c020ed0d6
BasicPanda <BasicPanda@JB>
2017-10-29 23:44:14 -0400

    Merge origin/master
    Conflicts:
    	food-mood/foodmood.db

--- food-mood/src/mood/AddMoodCntl.java
+++ food-mood/src/mood/AddMoodCntl.java
@@ -5,6 +5,8 @@ import database.Mood_Table;
 import database.User_Table;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JList;
 import models.Mood;
 
 /*
@@ -15,7 +17,7 @@ public class AddMoodCntl {
     private Mood mood;
     private final Mood_Table db;
     private final AddMoodView addMoodView;
-    public  String moodInput;
+    public String moodInput;
     public int moodSlider;
 
     public AddMoodCntl(Mood_Table db, AddMoodView addMoodView) {
@@ -42,15 +44,15 @@ public class AddMoodCntl {
             moodInput = addMoodView.getAddMoodPanel().getMField().getText();
             System.out.println("Mood: " + moodInput);
             moodSlider = addMoodView.getAddMoodPanel().getMSlider().getValue();
-            moodSlider/=10;
+            moodSlider /= 10;
             System.out.println("Mood Scale: " + moodSlider);
-            
+
             Mood mood = new Mood();
             mood.setName(moodInput);
             mood.setAmount(moodSlider);
-            
+
             db.addNewMood(mood);
-            
+
             addMoodView.dispose();
         }
     }
@@ -66,5 +68,12 @@ public class AddMoodCntl {
 
     public Mood_Table getDb() {
         return db;
+    }
+
+    public void updateList(MoodViewPanel moodViewPanel) {
+        ArrayList<Mood> temp = new ArrayList<>();
+        temp.add(db.getUserMood(103));
+        JList moods = new JList(temp.toArray());
+        moodViewPanel.setFoodListView(moods);
     }
 }
